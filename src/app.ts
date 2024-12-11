948b3d8abee097aaf5925c330a56232dbcd77eb6
Leonardo Lago <leodolago@gmail.com>
2024-12-11 14:02:39 -0300

    Add prisma

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
+import { PrismaClient } from "@prisma/client";
 import fastify from "fastify";
 
 export const app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: "Leonardo Lago",
+    email: "leonardo@gmail.com",
+  },
+})
